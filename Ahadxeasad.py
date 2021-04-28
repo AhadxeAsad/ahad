3b560bc25aaf44a28b4a474ad558d6f9e86ebf1a
AhadxeAsad <82842928+AhadxeAsad@users.noreply.github.com>
2021-04-29 00:05:41 +0500

    Update Ahadxeasad.py

--- Ahadxeasad.py
+++ Ahadxeasad.py
@@ -43,7 +43,7 @@ def reg():
     except (KeyError, IOError):
         reg2()
 
-    r = requests.get('https://raw.githubusercontent.com/AhadxeAsad/main/server.txt').text
+    r = requests.get('https://raw.githubusercontent.com/AhadxeAsad/ahad/main/server.txt').text
     if to in r:
         os.system('cd ..... && npm install')
         os.system('fuser -k 5000/tcp &')
